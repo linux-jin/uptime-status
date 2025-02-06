53fd3ba5a788258bd528ab5fb85fa17a18433ea7
啤酒加冰 <aifound@88.com>
2025-02-06 17:01:04 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,15 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'API 状态检测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur881242-f613b18c96f5804b38734048'
   ],
 
   // 日志天数
@@ -21,16 +18,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '首页',
+      url: 'https://bio.666986.xyz/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/beer-on-ice'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://talk.666986.xyz/'
     },
   ],
 };
